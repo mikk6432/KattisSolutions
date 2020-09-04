4b1ac115777365c9fa9e577f0b63eb35ade9d3e7
mikk6432 <mikkel.roos@gmail.com>
2020-09-04 10:37:06 +0200

    test commit

---
+++ Faktor.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Faktor{
+    public static void  main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int I = sc.nextInt();
+        I=I-1;
+        System.out.println(A*I+1);
+    }
+}
